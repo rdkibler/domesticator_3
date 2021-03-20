7a0f1ac039b55d8d90ffb07fddc184e5e4ac50b5
Ryan Kibler <rdkibler@gmail.com>
2021-03-20 14:47:33 -0700

    learning how to import my own scripts. Currently not working 100%

---
+++ tools/vector_parsing.py
@@ -0,0 +1,11 @@
+from Bio import SeqRecord
+
+def make_vector_record(vector_filepath) -> SeqRecord:
+	"""Return Biopython SeqRecord
+
+	Simply a wrapper around Biopython SeqIO machinery to read a single vector file
+
+	rdkibler 210320
+	"""
+	print("SUCCESS")
+	return SeqRecord()
