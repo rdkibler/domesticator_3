698b1cbba37e0cf84cbe1c0ec64e2f378f7a5798
Ryan Kibler <rdkibler@gmail.com>
2021-03-20 19:30:14 -0700

    ripped code from domesticator 1 for inserting sequence into vector

--- tools/input_parsing.py
+++ tools/input_parsing.py
@@ -1,3 +1,4 @@
+import copy
 import os
 from typing import Generator
 
@@ -36,6 +37,7 @@ def load_inserts(filenames) -> Generator[SeqRecord.SeqRecord,None,None]:
 	If the filename is a pdb file, then each chain is extracted and and they are 
 	returned together
 
+	rdkibler 210320
 	"""
 
 	for filename in filenames: 
@@ -66,11 +68,109 @@ def load_inserts(filenames) -> Generator[SeqRecord.SeqRecord,None,None]:
 def get_insert_locations(record) -> dict:
 	""" returns a dict of chain ID to dnachisel(?) locations
 
-	This function looks through the record for features of type "domesticator"
+	This function looks through the record for features of type "misc_feature"
 	and name matching the form "!insert(?)" where "?" is a chain letter. Returns a
 	dict of chain letter/ID to the location where it's supposed to go.
+
+	rdkibler 210320
 	"""
-	raise NotImplementedError()
+
+	location_dict = {}
+
+	for feature in record.features:
+		if feature.type == "misc_feature" and feature.qualifiers['label'][0].startswith("!insert("):
+			chain_letter = feature.qualifiers['label'][0].split("(")[1].split(")")[0]
+			location = feature.location
+			location_dict[chain_letter] = feature.location
+	return location_dict
+
+
+
+
+
+def replace_sequence_in_record(record, location, new_seq) -> SeqRecord.SeqRecord:
+	""" Returns a modified seqrecord
+
+	This function was borrowed from Domesticator1. It replaces the sequence in the record 
+	and (importantly) adjusts all the locations of the annotations if possible. It returns
+	the modified SeqRecord
+	"""
+
+	# I don't know if this is right. What does the strand number mean? --rdkibler 210320
+	if location.strand >= 0:
+		adjusted_seq = record.seq[:location.start] + new_seq.seq + record.seq[location.end:]
+	else:
+		adjusted_seq = record.seq[:location.start] + new_seq.reverse_complement().seq + record.seq[location.end:]
+
+	record.seq = adjusted_seq
+
+	seq_diff = len(new_seq) - len(location)
+	orig_start = location.start
+	orig_end = location.end
+
+	processed_features = []
+
+	for feat in record.features:
+
+		f_loc = feat.location
+
+		loc_list = []
+
+		for subloc in f_loc.parts:
+
+			assert(subloc.start <= subloc.end) 
+			
+			#type 1: where the start and end are contained within the original location
+			#-> do not add it to the processed_features list because I have no idea where it should go
+			if subloc.start > location.start and subloc.start < location.end and subloc.end > location.start and subloc.end < location.end:
+				continue
+
+			#type 1b: where the start and end are the same which will happen a lot for storing constraints and objectives
+			elif subloc.start == location.start and subloc.end == location.end:
+				new_loc = FeatureLocation(location.start, location.end + seq_diff, strand=subloc.strand)
+
+
+
+			#type 2: where they start or end inside the location
+			#-> chop off. don't forget to add on approprate amount
+			##THINK! does strand even matter? How is start and end defined? I'm assuming that for strand -1 things are flipped but that's 
+			##probably not how it's implemented. Also, consider strand = 0 (no direction). There is probably an easier way. 
+			elif subloc.start >= location.start and subloc.start <= location.end:
+				new_loc = FeatureLocation(location.end + seq_diff, subloc.end + seq_diff, strand=subloc.strand)
+
+			elif subloc.end >= location.start and subloc.end <= location.end:
+				new_loc = FeatureLocation(subloc.start, location.start, strand=subloc.strand)
+				
+
+			#type 3: where they span the location 
+			#-> keep the leftmost point same and add diff to rightmost. do not split
+			elif location.start >= subloc.start and location.start <= subloc.end and location.end >= subloc.start and location.end <= subloc.end:
+				new_loc = FeatureLocation(subloc.start, subloc.end + seq_diff, strand=subloc.strand)
+
+			#type 4: where they start and end before location
+			#-> add it to list unchanged
+			elif subloc.start <= location.start and subloc.end <= location.start:
+				new_loc = subloc
+
+			#type 5: where they start and end after location
+			#-> add diff to whole location
+			elif subloc.start >= location.end and subloc.end >= location.end:
+				new_loc = subloc + seq_diff
+
+			loc_list.append(new_loc)
+		
+		#if the list is empty, it means that all the sublocs were contained within the insert
+		if len(loc_list) > 0:
+			feat.location = sum(loc_list)
+			processed_features.append(feat)
+
+	record.features = processed_features
+
+	return record
+
+
+
+
 
 
 def put_insert_into_vector(insert_record,vector_record,location_ID:str) -> SeqRecord.SeqRecord:
@@ -79,6 +179,8 @@ def put_insert_into_vector(insert_record,vector_record,location_ID:str) -> SeqRe
 	This function will insert one insert_record into a copy of the vector_record at 
 	the specified location (a single letter), making sure to adjust other annotations
 	as needed.
+
+	rdkibler 210320
 	"""
 	raise NotImplementedError()
 
@@ -89,16 +191,17 @@ def make_naive_vector_records(base_vector_record, protein_filepaths) -> Generato
 	A generator which loads the proteins, randomly reverse translates each one, inserts them into the vectors.
 	and yields 
 
+	rdkibler 210320
 	"""
-	print('hi')
+
 	insert_locations = get_insert_locations(base_vector_record)
-	print('how')
+	print(insert_locations)
 	for inserts in load_inserts(protein_filepaths):
 		if len(insert_locations) == 1:
 			for insert in inserts:
 				yield put_insert_into_vector(insert,base_vector_record,"A")
 		else:
-			intermediate_vector_record = base_vector_record.copy()
+			intermediate_vector_record = copy.deepcopy(base_vector_record)
 			for insert in inserts:
 				intermediate_vector_record = put_insert_into_vector(insert, intermediate_vector_record, insert.annotations['chain'])
 			yield intermediate_vector_record
